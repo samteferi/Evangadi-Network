328fc1c5cab610d7938b3a3bc6012649ad0da571
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-03-09 18:50:12 -0500

    uninstall axios

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 const userRouter = require('./server/api/users/user.router');
 const questionRouter = require('./server/api/questions/question.router');
 const answerRouter = require('./server/api/answers/answer.router');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/users", userRouter);
