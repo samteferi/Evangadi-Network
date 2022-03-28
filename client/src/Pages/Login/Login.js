fc73392644967f0203944d61e5e01b9ce9ca8c8f
Rediat <red.terefe@gmial.com>
2022-03-29 01:56:09 +0300

    feat(login): link created to go to  signup

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../context/UserContext';
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 
 const Login = () => {
   const [userData, setUserData] = useContext(UserContext);
@@ -9,49 +9,52 @@ const Login = () => {
   const [form, setForm] = useState({});
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
-  }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const loginRes = await axios.post('http://localhost:4000/api/users/login',
+      const loginRes = await axios.post(
+        "http://localhost:4000/api/users/login",
         {
           email: form.email,
-          password: form.password
-        });
-        console.log(loginRes)
+          password: form.password,
+        }
+      );
+      console.log(loginRes);
       setUserData({
         token: loginRes.data.token,
-        user: loginRes.data.user
+        user: loginRes.data.user,
       });
 
-      localStorage.setItem('auth-token', loginRes.data.token);
-      navigate('/');
+      localStorage.setItem("auth-token", loginRes.data.token);
+      navigate("/");
     } catch (err) {
-      console.log('problem', err);
+      console.log("problem", err);
     }
-  }
+  };
 
   useEffect(() => {
-    if (userData.user) navigate('/');
+    if (userData.user) navigate("/");
   }, [userData.user, navigate]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Email: </label>
-      <input
-        type="text"
-        name="email"
-        onChange={handleChange}
-      /><br />
-      <label>Password: </label>
-      <input
-        type="password"
-        name="password"
-        onChange={handleChange}
-      /><br />
-      <button>submit</button>
-    </form>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Email: </label>
+        <input type="text" name="email" onChange={handleChange} />
+        <br />
+        <label>Password: </label>
+        <input type="password" name="password" onChange={handleChange} />
+        <br />
+        <button>submit</button>
+      </form>
+      <div>
+        <Link to='/signup'>
+              Already have an account?
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
