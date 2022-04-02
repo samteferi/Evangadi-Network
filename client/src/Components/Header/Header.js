929dbc7e0a863fdc6dedbf683f85c841bdb558bc
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-04-02 19:15:33 -0400

    style(Header): fix styling on header with flex-box

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -16,17 +16,17 @@ function Header1({ logout }) {
     navigate("/login");
   }
   return (
-    <div className="header">
-      <div className="innerContainer  d-flex  text-center  justify-content-around ">
-        <Link to='/' className="header__image text-center">
+    <div className="header container-fluid">
+      <div className="innerContainer container d-flex justify-content-around ">
+        <Link to='/' className="header__image">
           <img src={logo} alt="Evangadi logo"/>
         </Link>
         <button className='ic d-sm-block d-md-none'>
                ☰
         </button>
-        <div className="d-flex  innerContainer2 text-center pt-3 justify-content-between">
-          <a className="pt-2" href="">Home</a>
-          <a className="pt-2" href="">How it Works</a>
+        <div className="d-flex  innerContainer2 justify-content-between">
+          <a href="">Home</a>
+          <a href="">How it Works</a>
           <div className="rightSide__button">
             <button className="btn_header" onClick={goToSignIn}>{userData.user? "LogOut" : "SIGN IN"}</button>
           </div>
