1cad3f59fe871db7fe9e1c6d675c23079eae8749
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-04-04 00:16:31 -0400

    style updated

--- client/src/Components/QuestionsNew/QuestionsNew.js
+++ client/src/Components/QuestionsNew/QuestionsNew.js
@@ -5,15 +5,15 @@ import profile from "../../Images/Male User.png"
 const QuestionsNew = ({question}) => {
     return (
         <div className="d-md-flex align-items-center justify-space-between">
-            <div className="d-flex flex-md-column">
+            <div className="d-flex flex-md-column avatar-container">
                 <img className="avatar" src={profile}alt="Avatar" />
-                <h6 className="align-self-center ms-2 ms-md-0">USER NAME</h6>
+                <h6 className="align-self-center ms-2 ms-md-0 text-center">USER NAME</h6>
             </div>
             <div className="ms-md-5 flex-grow-1">
                 <h6 className="pt-2 pt-md-0">{question}</h6>
             </div>
             <div className="d-none d-md-block ms-md-5">
-                <i class="fa fa-angle-right "></i>
+                <i className="fa fa-angle-right "></i>
             </div>
         </div>
     )
