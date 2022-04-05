b5210294fc8c26193f2decde8b8c14a4452cf964
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-04-05 14:23:06 -0400

    update: style of homepage

--- client/src/Pages/SignUp/SignUp.js
+++ client/src/Pages/SignUp/SignUp.js
@@ -37,99 +37,101 @@ const SignUp = () => {
   };
 
   const [type, setType] = useState("password");
-  
+
   // to change type attribute from 'password' to 'text' and vice versa
- const [icon, setIcon] = useState(eyeOff);
- // to change the icon when clicked
- const HandleIconChange = () => {
-   // event listenforPassworder function
-   if (type === "password") {
-     setIcon(eye);
-     setType("text");
-   } else {
-     setIcon(eyeOff);
-     setType("password");
-   }
- };
+  const [icon, setIcon] = useState(eyeOff);
+  // to change the icon when clicked
+  const HandleIconChange = () => {
+    // event listenforPassworder function
+    if (type === "password") {
+      setIcon(eye);
+      setType("text");
+    } else {
+      setIcon(eyeOff);
+      setType("password");
+    }
+  };
   return (
-    <div className="container  d-md-flex mx-auto my-5 signup_container">
-      <div className="containe mainn">
-        <p className="p11">Join the network</p>
-        <p className="p22 lorem">
-          Already have an account?
-          <Link to="/login" className="a11">
-            Sign in
-          </Link>
-        </p>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="in11 mr-1 in11i "
-            name="email"
-            onChange={handleChange}
-            type="email"
-            placeholder="Email"
-          />
-          <div className="FLname d-flex">
+    <div className="container-fluid sign_page">
+      <div className="container  d-md-flex mx-auto py-5 signup_container">
+        <div className="containe mainn">
+          <p className="p11">Join the network</p>
+          <p className="p22 lorem">
+            Already have an account?
+            <Link to="/login" className="a11">
+              Sign in
+            </Link>
+          </p>
+          <form onSubmit={handleSubmit}>
             <input
-              className="in11 in11F"
-              name="firstName"
+              className="in11 mr-1 in11i "
+              name="email"
+              onChange={handleChange}
+              type="email"
+              placeholder="Email"
+            />
+            <div className="FLname d-flex">
+              <input
+                className="in11 in11F"
+                name="firstName"
+                onChange={handleChange}
+                type="text"
+                placeholder="First Name"
+              />
+
+              <input
+                className="in11 in11L"
+                name="lastName"
+                onChange={handleChange}
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+
+            <input
+              className="in11 in11i"
+              name="userName"
               onChange={handleChange}
               type="text"
-              placeholder="First Name"
+              placeholder="User Name"
             />
 
             <input
-              className="in11 in11L"
-              name="lastName"
+              className="in11 in11i"
               onChange={handleChange}
-              type="text"
-              placeholder="Last Name"
+              name="password"
+              type={type}
+              placeholder="Password"
             />
-          </div>
+            <span onClick={HandleIconChange} className="showHide">
+              <Icon icon={icon} size={20} />
+            </span>
+          </form>
+          <p className="mt-md-5 mt-sm-5  texttag">
+            I agree to the
+            <Link to="" className="a22">
+              privacy policy
+            </Link>
+            and
+            <Link to="" className="a22">
+              terms of serivice.
+            </Link>
+          </p>
+          <button className="btnSign">Agree and Join</button>
 
-          <input
-            className="in11 in11i"
-            name="userName"
-            onChange={handleChange}
-            type="text"
-            placeholder="User Name"
-          />
-
-          <input
-            className="in11 in11i"
-            onChange={handleChange}
-            name="password"
-            type={type}
-            placeholder="Password"
-          />
-        <span onClick={HandleIconChange} className="showHide">
-           <Icon icon={icon} size={20}/>
-        </span>
-        </form>
-        <p className="mt-md-5 mt-sm-5  texttag">
-          I agree to the
-          <Link to="" className="a22">
-            privacy policy
+          <Link to="/login" className="a33">
+            Already have an account?
           </Link>
-          and
-          <Link to="" className="a22">
-            terms of serivice.
-          </Link>
-        </p>
-        <button className="btnSign">Agree and Join</button>
-
-        <Link to="/login" className="a33">
-          Already have an account?
-        </Link>
-      </div>
-      <div className="SignupNote container mt-sm-5">
+        </div>
+        <div className="SignupNote container mt-sm-5">
           <p className="forTitle">About</p>
           <h1>Evangadi Networks Q&A</h1>
           <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem voluptate officiis beatae nobis pariatur omnis facere accusamus laboriosam hic, adipisci vero reiciendis, recusandae sit ad, eum quisquam! Molestias, ut commodi!</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem voluptate officiis beatae nobis pariatur omnis facere accusamus laboriosam hic, adipisci vero reiciendis, recusandae sit ad, eum quisquam! Molestias, ut commodi!</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor odio harum sunt, quaerat, molestias fuga expedita ad excepturi officiis aliquam aut nemo ratione culpa id laborum ipsum porro tempore?</p>
           <button className="btn1">HOW IT WORKS</button>
-    </div>
+        </div>
+      </div>
     </div>
   );
 };
