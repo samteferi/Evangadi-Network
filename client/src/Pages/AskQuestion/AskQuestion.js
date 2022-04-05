15df720d3cbfd8b848d0e6893222d2ec17c6a3eb
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-04-05 17:02:47 -0400

    feat: ask question implemented succuess

--- client/src/Pages/AskQuestion/AskQuestion.js
+++ client/src/Pages/AskQuestion/AskQuestion.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./AskQuestion.css";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import axios from "axios";
 
 export default function AskQuestion() {
-  
-  return (   
+  const [userData, setUserData] = useContext(UserContext);
+  const [form, setForm] = useState({});
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:4000/api/questions",
+        {
+          id:userData.user.id,
+          question: form.question,
+          questionDescription: form.questionDescription
+        }
+      );
+      navigate("/");
+    } catch (err) {
+      console.log("problem", err);
+    }
+  }
+  return (
     <div className="container my-5">
       <div className="d-flex flex-column align-items-center my-5">
         <h3>Steps to write a good question</h3>
@@ -16,17 +37,17 @@ export default function AskQuestion() {
           <li>Review your question and post it to the site.</li>
         </ul>
       </div>
-      <form className="d-flex flex-column p-5 question_form  justify-content-between">
-        <h3 className="">Ask a public question</h3>
+      <form onSubmit={handleSubmit} className="d-flex flex-column p-5 question_form  justify-content-between">
+        <h3>Ask a public question</h3>
         <Link to="/" className="text-decoration-none text-reset cursor-pointer">
           Go to Question page
         </Link>
-        <input className="question_title" type="text" name="" Placeholder="Title"/>
+        <input className="question_title" type="text" name="question" Placeholder="Title" onChange={handleChange} />
         <textarea
           className="question_input"
           placeholder="Question Description..."
-          name=""
-          id=""
+          name="questionDescription"
+          onChange={handleChange}
         ></textarea>
         <button className="question_post_btn" type="">
           Post Your Question
