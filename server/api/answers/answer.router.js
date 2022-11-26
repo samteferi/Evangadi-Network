f7a57713a0d28b75b5f50b49c959a0d0c05b26d8
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-11-26 16:48:09 -0500

    back-end deleted

--- server/api/answers/answer.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { solveQuestion, getAnswerByQuestionId } = require('./answer.controller');
-
-router.post('/', solveQuestion);
-router.get('/:id', getAnswerByQuestionId);
-
-module.exports = router;
