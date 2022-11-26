a76b182ce954fc4bd8c8fe206bb56010483c81fc
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-11-26 18:37:09 -0500

    url update

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    // baseURL: "https://evangadi-network-server-production.up.railway.app/",
-    baseURL: "http://localhost:4000/",
+    baseURL: "https://evangadi-network-server-production.up.railway.app/",
+    // baseURL: "http://localhost:4000/",
 });
 export default instance;
